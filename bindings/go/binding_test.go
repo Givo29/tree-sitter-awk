cf24385aada4a921cf664c762a39e32c14a9ac98
Joel Giovinazzo <joelgiovinazzo@gmail.com>
2024-09-18 16:16:24 +1000

    Update binding_test.go

--- bindings/go/binding_test.go
+++ bindings/go/binding_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 
 	tree_sitter "github.com/smacker/go-tree-sitter"
-	"github.com/tree-sitter/tree-sitter-awk"
+	"github.com/givo29/tree-sitter-awk"
 )
 
 func TestCanLoadGrammar(t *testing.T) {
