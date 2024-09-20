55a2a7ebae20405cbe6f3653261382a378c645fe
Joel Giovinazzo <joelgiovinazzo@gmail.com>
2024-09-20 14:13:42 +1000

    Not really sure what I'm doing

--- bindings/go/binding_test.go
+++ bindings/go/binding_test.go
@@ -3,7 +3,7 @@ package tree_sitter_awk_test
 import (
 	"testing"
 
-	tree_sitter "github.com/smacker/go-tree-sitter"
+	tree_sitter "github.com/tree-sitter/go-tree-sitter"
 	"github.com/givo29/tree-sitter-awk"
 )
 
--- go.mod
+++ go.mod
@@ -1,8 +1,6 @@
-module github.com/Givo29/tree-sitter-awk
+module github.com/givo29/tree-sitter-awk
 
-go 1.23
-
-toolchain go1.23.0
+go 1.23.0
 
 require github.com/tree-sitter/go-tree-sitter v0.23.1
 
